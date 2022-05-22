c2c17d7bcebaa5dbaee6d042ff7dc204dcdbc7d3
Michael <mishoopop6@gmail.com>
2022-05-22 21:48:01 +0200

    initial commit

---
+++ Games-Rental-System/Form1.cs
@@ -0,0 +1,10 @@
+namespace Games_Rental_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Games-Rental-System/Program.cs
@@ -0,0 +1,15 @@
+namespace Games_Rental_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
