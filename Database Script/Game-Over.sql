8a556dddc1e3e9e9f6c8313215e388b6983ef084
Michael Maher <michael.maher.nagi@gmail.com>
2022-06-03 07:27:56 +0200

    final push

---
+++ Database Script/Game-Over.sql
@@ -0,0 +1,181 @@
+/*==============================================================*/
+/* DBMS name:      Microsoft SQL Server 2017                    */
+/* Created on:     5/29/2022 8:48:35 PM                         */
+/*==============================================================*/
+
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('GAME') and o.name = 'FK_GAME_MANAGE_ADMIN')
+alter table GAME
+   drop constraint FK_GAME_MANAGE_ADMIN
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('RENT') and o.name = 'FK_RENT_RENT_CLIENT')
+alter table RENT
+   drop constraint FK_RENT_RENT_CLIENT
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('RENT') and o.name = 'FK_RENT_RENT2_GAME')
+alter table RENT
+   drop constraint FK_RENT_RENT2_GAME
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ADMIN')
+            and   type = 'U')
+   drop table ADMIN
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('CLIENT')
+            and   type = 'U')
+   drop table CLIENT
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('GAME')
+            and   name  = 'MANAGE_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index GAME.MANAGE_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('GAME')
+            and   type = 'U')
+   drop table GAME
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('RENT')
+            and   name  = 'RENT2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index RENT.RENT2_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('RENT')
+            and   name  = 'RENT_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index RENT.RENT_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('RENT')
+            and   type = 'U')
+   drop table RENT
+go
+
+/*==============================================================*/
+/* Table: ADMIN                                                 */
+/*==============================================================*/
+create table ADMIN (
+   A_USERNAME           varchar(80)          not null,
+   A_PASSWORD           varchar(50)          not null,
+   constraint PK_ADMIN primary key (A_USERNAME)
+)
+go
+
+/*==============================================================*/
+/* Table: CLIENT                                                */
+/*==============================================================*/
+create table CLIENT (
+   C_USERNAME           varchar(80)          not null,
+   C_PASSWORD           varchar(50)          not null,
+   C_FIRSTNAME          varchar(50)          not null,
+   C_LASTNAME           varchar(50)          not null,
+   C_PHONENUM           varchar(11)          not null,
+   constraint PK_CLIENT primary key (C_USERNAME)
+)
+go
+
+/*==============================================================*/
+/* Table: GAME                                                  */
+/*==============================================================*/
+create table GAME (
+   G_NAME               varchar(100)         not null,
+   A_USERNAME           varchar(80)          null,
+   V_NAME               varchar(50)          not null,
+   G_CATEGORY           varchar(50)          not null,
+   G_AMOUNT             int                  not null,
+   G_PRICE              float                not null,
+   G_DATE               datetime             not null,
+   G_PHOTO              image                not null,
+   constraint PK_GAME primary key (G_NAME)
+)
+go
+
+/*==============================================================*/
+/* Index: MANAGE_FK                                             */
+/*==============================================================*/
+
+
+
+
+create nonclustered index MANAGE_FK on GAME (A_USERNAME ASC)
+go
+
+/*==============================================================*/
+/* Table: RENT                                                  */
+/*==============================================================*/
+create table RENT (
+   R_ID                 int                  not null identity(1,1),
+   C_USERNAME           varchar(80)          not null,
+   G_NAME               varchar(100)         not null,
+   RENT_DATE            datetime             not null,
+   RETURN_DATE          datetime             null,
+   constraint PK_RENT primary key (R_ID)
+)
+go
+
+/*==============================================================*/
+/* Index: RENT_FK                                               */
+/*==============================================================*/
+
+
+
+
+create nonclustered index RENT_FK on RENT (C_USERNAME ASC)
+go
+
+/*==============================================================*/
+/* Index: RENT2_FK                                              */
+/*==============================================================*/
+
+
+
+
+create nonclustered index RENT2_FK on RENT (G_NAME ASC)
+go
+
+alter table GAME
+   add constraint FK_GAME_MANAGE_ADMIN foreign key (A_USERNAME)
+      references ADMIN (A_USERNAME)
+go
+
+alter table RENT
+   add constraint FK_RENT_RENT_CLIENT foreign key (C_USERNAME)
+      references CLIENT (C_USERNAME)
+go
+
+alter table RENT
+   add constraint FK_RENT_RENT2_GAME foreign key (G_NAME)
+      references GAME (G_NAME)
+go
+
+INSERT [dbo].[ADMIN] ([A_USERNAME], [A_PASSWORD]) VALUES (N'michael', N'michael1234')
+GO
--- README.md
+++ README.md
@@ -17,10 +17,12 @@ The admin of the system is responsible for adding the games to be rented out by 
 
 <h2>Project Steps</h2>
 <h3>First: </h3>
-We draw the ERD for this project using <b>PowerDesigner</b>
+We draw the ERD for this project using <b>PowerDesigner</b></br>
+Here is the link of the --> <a href="https://github.com/Michael-M-aher/Games-Rental-System/ERD">ERD</a></br>
 
 <h3>Second: </h3>
-We converted the ERD to physical model (DDL scripts)
+We converted the ERD to physical model (DDL scripts)</br>
+Here is the link of the --> <a href="https://github.com/Michael-M-aher/Games-Rental-System/Database Script/Game-Over.sql">SQL Script</a></br>
 
 <h3>Third: </h3>
 
@@ -28,13 +30,39 @@ We developped the software application using C# and connected the database to be
 
 <h2>Some screenshots: </h2>
 
-![image](https://user-images.githubusercontent.com/74511706/171738486-ce4a9321-e7b9-4306-8c09-2685149ebd3a.png)
+![image](https://user-images.githubusercontent.com/74511706/171738818-2da87538-889b-42d0-b83f-55c12821235b.png)
 
-![image](https://user-images.githubusercontent.com/74511706/171738644-feae84d6-3865-4bb8-aefd-1f76dc03dd56.png)
+![image](https://user-images.githubusercontent.com/25803558/171791668-c739787c-4966-4faa-877a-1e98954d8286.png)
+
+![image](https://user-images.githubusercontent.com/25803558/171791667-6a7caa99-20ca-4184-94a7-5f1ed0186ff0.png)
 
 ![image](https://user-images.githubusercontent.com/25803558/171775152-9eaf75cd-def4-455d-b0b7-9c4ab0e8c2ae.png)
 
-![image](https://user-images.githubusercontent.com/74511706/171738818-2da87538-889b-42d0-b83f-55c12821235b.png)
+![image](https://user-images.githubusercontent.com/25803558/171792041-4e6f7337-36e7-4a71-a0a3-d3ab4ca83648.png)</br></br>
 
-![image](https://user-images.githubusercontent.com/74511706/171738991-5e2173e6-7eee-4eb8-8308-e9305af7cf45.png)
+Setting up the environment 🛠
+--------------------------
+
+#### 1. Create a database in Microsoft SQL Server and name it 'Game-Over'.
+
+#### 2. Open the SQL Script and execute all the Queries in Game-Over database.
+
+#### 3. Open the Solution file and change the 'Data Source' value in the _sqlcon string in "sqlCon.cs" file to the name of your server.
+
+```
+internal class sqlCon
+    {
+        static string _sqlcon = @"Data Source=SeverName; Initial Catalog=Game-Over; Integrated Security=True";
+        public static string sqlcon
+        {
+            get => _sqlcon;
+        }
+    }
+```
+
+#### 3. You have successfully created the database and linked it to the C# Code.</br></br>
+
+<b>To Run the Program here is the default user name and password:</b></br>
+username: michael</br>
+password: michael1234
 
